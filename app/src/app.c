c3409f9abdc8ae44bd089a0fc042d07bfbc9620a
axeltantalean1 <axel.tantalean@alu.ing.unlp.edu.ar>
2024-10-24 22:56:18 -0300

    prueba lcd

--- app/src/app.c
+++ app/src/app.c
@@ -10,6 +10,7 @@
 #include "sapi.h"        // <= Biblioteca sAPI
 #include "chip.h"
 #include "sapi_esp8266.h"
+#include "sapi_lcd.h"
 
 // FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
 int main( void )
@@ -18,19 +19,27 @@ int main( void )
 
    // Inicializar y configurar la plataforma
    boardConfig();
-
    // Crear varias variables del tipo booleano
    bool_t buttonValue = OFF;
-   bool_t ledValue    = OFF;
-   // Crear variable del tipo tick_t para contar tiempo
-   tick_t timeCount   = 0;
-   Chip_EEPROM_Init(LPC_EEPROM);
-   esp8266InitHttpServer("a", "a");
+   lcdInit(16,2,5,8);
+   lcdGoToXY(0,0);
+   int x=0;
+   lcdSendStringRaw("Cargando...     ");
    // ---------- REPETIR POR SIEMPRE --------------------------
    while( TRUE ) {
-      
-      
-      
+      if(x < 16){
+         lcdGoToXY(x,1);
+         lcdSendStringRaw("+");
+         x++;
+      }
+      else{
+         lcdClear();
+         lcdGoToXY(0,0);
+         lcdSendStringRaw("Funciona bien pa");
+         lcdGoToXY(0,1);
+         lcdSendStringRaw("................");
+      }
+      delay(500);
    }
 
    // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
