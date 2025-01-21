39a58b990c8a9e71bec58b3f1ea442f0fe963c92
Nicolas Colombini <nicolascolombini44@gmail.com>
2025-01-21 14:30:03 -0300

    feat: limpiando el main

--- app/src/app.c
+++ app/src/app.c
@@ -19,21 +19,9 @@ int main( void )
    // Inicializar y configurar la plataforma
    boardConfig();
 
-   HX711_Init(T_FIL1, T_COL2);
-   printf("Comenzando lecturas");
-   int32_t data; // Variable para almacenar el dato leído
-   int8_t leyo;
-   gpioWrite(LED1, HIGH);
    // ---------- REPETIR POR SIEMPRE --------------------------
    while( TRUE ) {
-      leyo = HX711_Read(&data);
-      if (leyo) {
-         printf("Valor leído: %ld\n", data);
-      }
-      printf("Leyo: %hhd\n", leyo); 
-      gpioToggle(LED1);
-      delay(1000);
-      
+ 
    }
 
    // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
