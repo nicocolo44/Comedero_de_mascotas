daf218add200040fa1ca6ee9f1b75e5c01b49b91
Nicolas Colombini <nicolascolombini44@gmail.com>
2025-01-23 02:34:42 -0300

    feat: Agregando timer

--- app/src/app.c
+++ app/src/app.c
@@ -11,6 +11,11 @@
 #include "chip.h"
 #include "sapi_esp8266.h"
 
+void timerCallback(void) {          
+    gpioToggle(LED1);     // Actualizar el estado del LED1
+}
+
+
 // FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
 int main( void )
 {
@@ -18,7 +23,7 @@ int main( void )
 
    // Inicializar y configurar la plataforma
    boardConfig();
-
+   Timer_Init( 0, Timer_microsecondsToTicks( 1000000 ), timerCallback );
    // ---------- REPETIR POR SIEMPRE --------------------------
    while( TRUE ) {
  
